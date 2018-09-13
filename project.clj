1fd9ef7609179944e100cb5a09adbff5447f72d2
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-13 17:58:43 +0200

    Formatting code.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject org.vladimir/utils-lib "0.1.0"
-  :description    "Utils library"
-  :url            "http://gitlab:1610/VladimirMarkovic86/utils-lib"
-  :license        {:name "Eclipse Public License"
-                   :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies   [[org.clojure/clojure    "1.9.0"]
-                   [org.clojure/clojurescript "1.10.339"]
-                   ]
+  :description "Utils library"
+  :url "http://gitlab:1610/VladimirMarkovic86/utils-lib"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.339"]
+                 ]
   :source-paths ["src/cljc"])
 
