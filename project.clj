ff48fa7dfad414e5969a62357049a21df11704be
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:48:33 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject org.vladimir/utils-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/utils-lib "0.1.0"
   :description "Utils library"
-  :url "http://gitlab:1610/VladimirMarkovic86/utils-lib"
+  :url "http://github.com/VladimirMarkovic86/utils-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
                  ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/cljc"])
 
